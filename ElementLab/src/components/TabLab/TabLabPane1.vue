0278ce9b049168ba3f09b8b6f83d265d60fe3641
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-07-21 15:39:43 +0800

    测试生命周期勾子

--- ElementLab/src/components/TabLab/TabLabPane1.vue
+++ ElementLab/src/components/TabLab/TabLabPane1.vue
@@ -11,8 +11,23 @@ export default {
   data() {
     return {};
   },
+  beforeCreate: function() {
+    console.log("beforeCreate1");
+  },
+  created: function() {
+    console.log("created1");
+  },
+  beforeMounte: function() {
+    console.log("beforeMounte1");
+  },
   mounted: function() {
     console.log("TabLabPane1");
+  },
+  beforeDestroy: function() {
+    console.log("beforeDestroy1");
+  },
+  destroyed: function() {
+    console.log("destroyed1");
   }
 };
 </script>
--- ElementLab/src/components/TabLab/TabLabPane2.vue
+++ ElementLab/src/components/TabLab/TabLabPane2.vue
@@ -11,8 +11,23 @@ export default {
   data() {
     return {};
   },
+  beforeCreate: function() {
+    console.log("beforeCreate2");
+  },
+  created: function() {
+    console.log("created2");
+  },
+  beforeMounte: function() {
+    console.log("beforeMounte2");
+  },
   mounted: function() {
     console.log("TabLabPane2");
+  },
+  beforeDestroy: function() {
+    console.log("beforeDestroy2");
+  },
+  destroyed: function() {
+    console.log("destroyed2");
   }
 };
 </script>
