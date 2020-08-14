18da053d2814c2d0211e9bdc052e8981807ec0e9
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-08-14 11:06:14 +0800

    测试 v-if 也不行

--- ElementLab/src/components/DialogLab/index.vue
+++ ElementLab/src/components/DialogLab/index.vue
@@ -15,21 +15,30 @@
     <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
 
     <!-- 测试 destroy-on-close 没有用不知道为什么 -->
-    <el-dialog title="收货地址" :visible.sync="dialogFormVisible" @closed="handlerClosed">
-      <el-form ref="form" :model="form">
-        <el-form-item label="活动名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="活动区域" :label-width="formLabelWidth">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    <el-dialog
+      title="收货地址"
+      :visible.sync="dialogFormVisible"
+      @closed="handlerClosed"
+      :destroy-on-close="true"
+      v-if="ifVisible"
+    >
+      <div>
+        <input type="text" :value="testNumber" />
+        <el-form ref="form" :model="form">
+          <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
       </div>
     </el-dialog>
   </div>
@@ -74,7 +83,18 @@ export default {
         desc: "",
       },
       formLabelWidth: "120px",
+      index: 1,
     };
+  },
+  computed: {
+    ifVisible: function() {
+      console.log(`if---${this.dialogFormVisible}`)
+      return this.dialogFormVisible;
+    },
+    testNumber: function () {
+      console.log("触发 testNumber");
+      return this.index + Math.ceil(Math.random() * 10);
+    },
   },
   methods: {
     handlerClosed() {
