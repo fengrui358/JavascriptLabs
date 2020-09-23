b48a70978dd92e51f4c5db35a592b2f595ffead2
fengrui <fengrui358@163.com>
2020-09-23 22:59:20 +0800

    导航参数

--- VueJsLabs/jikeshijian-antdesignpro/ant-design-vue-pro/src/layouts/SiderMenu.vue
+++ VueJsLabs/jikeshijian-antdesignpro/ant-design-vue-pro/src/layouts/SiderMenu.vue
@@ -4,14 +4,17 @@
       <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
     </a-button>
     <a-menu
-      :default-selected-keys="['1']"
-      :default-open-keys="['2']"
+      :selected-keys="selectedKeys"
+      :open-keys.sync="openKeys"
       mode="inline"
       :theme="theme"
-      :inline-collapsed="collapsed"
     >
       <template v-for="item in menuData">
-        <a-menu-item v-if="!item.children" :key="item.path">
+        <a-menu-item
+          v-if="!item.children"
+          :key="item.path"
+          @click="$router.push({path: item.path, query: $route.query})"
+        >
           <a-icon :type="item.meta.icon" v-if="item.meta.icon" />
           <span>{{ item.meta.title }}</span>
         </a-menu-item>
@@ -32,27 +35,50 @@ export default {
       default: "dark"
     }
   },
+  watch: {
+    "$route.path": function(val) {
+      this.selectedKeys = this.selectKeysMap[val];
+      this.openKeys = this.collapsed ? [] : this.openKeysMap[val];
+    }
+  },
   data() {
+    this.selectKeysMap = {};
+    this.openKeysMap = {};
     const menuData = this.getMenuData(this.$router.options.routes);
     return {
       collapsed: false,
-      menuData
+      menuData,
+      selectedKeys: this.selectKeysMap[this.$router.path],
+      openKeys: this.collapsed ? [] : this.openKeysMap[this.$route.path]
     };
   },
+  created() {},
   methods: {
     toggleCollapsed() {
       this.collapsed = !this.collapsed;
     },
-    getMenuData(routes) {
+    getMenuData(routes = [], parentKeys = [], selectedKey) {
       const menuData = [];
 
       routes.forEach(item => {
         if (item.name && !item.hideInMenu) {
+          this.openKeysMap[item.path] = parentKeys;
+          this.selectKeysMap[item.path] = [item.path || selectedKey];
+
           const newItem = { ...item };
           delete newItem.children;
 
           if (item.children && !item.hideChildrenInMenu) {
-            newItem.children = this.getMenuData(item.children);
+            newItem.children = this.getMenuData(item.children, [
+              ...parentKeys,
+              item.path
+            ]);
+          } else {
+            this.getMenuData(
+              item.children,
+              selectedKey ? parentKeys : [...parentKeys, item.path],
+              selectedKey || item.path
+            );
           }
 
           menuData.push(newItem);
--- VueJsLabs/jikeshijian-antdesignpro/ant-design-vue-pro/src/layouts/SubMenu.vue
+++ VueJsLabs/jikeshijian-antdesignpro/ant-design-vue-pro/src/layouts/SubMenu.vue
@@ -5,7 +5,11 @@
       <span>{{ props.menuInfo.meta.title }}</span>
     </span>
     <template v-for="item in props.menuInfo.children">
-      <a-menu-item v-if="!item.children" :key="item.path">
+      <a-menu-item
+        v-if="!item.children"
+        :key="item.path"
+        @click="parent.$router.push({path: item.path, query: parent.$route.query})"
+      >
         <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
         <span>{{ item.meta.title }}</span>
       </a-menu-item>
@@ -16,6 +20,6 @@
 
 <script>
 export default {
-  props: ['menuInfo']
+  props: ["menuInfo"]
 };
 </script>
