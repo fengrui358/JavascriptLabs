b0992b4369935d0ce126088d0e9ff17fce542c54
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-11-16 16:38:59 +0800

    add

--- ElementLab/src/App.vue
+++ ElementLab/src/App.vue
@@ -12,6 +12,25 @@ export default {
   name: "App",
   components: {
     Layout
+  },
+  //测试生命周期异步方法
+  async created(){
+    console.log("created start");
+
+    await new Promise(resolve => {
+      setTimeout(()=>resolve(console.log("created resolved")), 1500);
+    });
+
+    console.log("created end");
+  },
+  async mounted(){
+    console.log("mounted start");
+
+    await new Promise(resolve => {
+          setTimeout(()=>resolve(console.log("mounted resolved")), 1000);
+        });
+
+    console.log("mounted end");
   }
 };
 </script>
