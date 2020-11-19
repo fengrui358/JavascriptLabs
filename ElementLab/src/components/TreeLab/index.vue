5f2e049a031903b7a768d24312b0a3fee1762ada
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-11-19 10:10:00 +0800

    优化ui

--- ElementLab/src/components/TreeLab/index.vue
+++ ElementLab/src/components/TreeLab/index.vue
@@ -158,7 +158,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .custom-tree-container {
   width: 600px;
 }
@@ -170,7 +170,10 @@ export default {
   justify-content: space-between;
   font-size: 14px;
   padding-right: 8px;
-  
+
+  >span:first-of-type {
+    margin-right: 8px;
+  }
 }
 
 .block {
