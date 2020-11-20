5a589d76c1fdd3de4bb710923f98731c52baacc6
fengrui <fengrui358@163.com>
2020-11-20 22:25:16 +0800

    modify

--- Nodejs/jikeshijian/13/nonblocking/index.js
+++ Nodejs/jikeshijian/13/nonblocking/index.js
@@ -1,6 +1,16 @@
 const glob = require('glob')
 
-console.time('glob')
+//阻塞IO调用
+console.time('sync glob')
 let result = glob.sync(__dirname + '/**/*')
-console.timeEnd('glob')
+
+console.timeEnd('sync glob')
 console.log(result)
+
+//非阻塞IO调用
+console.time('async glob')
+glob(__dirname + '/**/*', function (err, res) {
+    result = res
+    console.log(result)
+})
+console.timeEnd('async glob')
