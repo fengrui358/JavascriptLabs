9020ec5095ebd9b2c835b9ccf58337cc78f5f223
fengrui <fengrui358@163.com>
2020-11-24 23:27:09 +0800

    add

---
+++ Nodejs/jikeshijian/19/httpServer/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (request, response) {
+    if (request.url == '/favicon.ico') {
+        response.writeHead(200);
+        response.end('hello');
+        return;
+    }
+    else {
+        console.log(request);
+
+        response.writeHead(200);
+        fs.createReadStream(__dirname + '/index.html').pipe(response);
+    }
+}).listen(80)
