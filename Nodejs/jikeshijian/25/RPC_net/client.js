af6883bb6b15c4fe47bf07139840b146edc51998
free <fengrui358@163.com>
2020-11-27 22:21:26 +0800

    add

--- Nodejs/jikeshijian/25/RPC_net/client.js
+++ Nodejs/jikeshijian/25/RPC_net/client.js
@@ -3,8 +3,41 @@ const net = require('net');
 const socket = new net.Socket({});
 
 socket.connect({
-    host: '127.0.0.1',
+    host: '192.168.1.80',
     port: 4000
 });
 
-socket.write('hello world');
+socket.on('data', buffer => {
+    console.log(buffer, buffer.toString());
+});
+
+const lessonids = [
+    136797,
+    136798,
+    136799,
+    136800,
+    136801,
+    136803,
+    136804,
+    136806,
+    136807,
+    136808,
+    136809,
+    141994,
+    143517,
+    143557,
+    143564,
+    143644,
+    146470,
+    146569,
+    146582,
+];
+
+setInterval(() => {
+    let buffer = Buffer.alloc(4);
+    buffer.writeInt32BE(
+        lessonids[Math.floor(Math.random() * lessonids.length)]
+    );
+
+    socket.write(buffer);
+}, 2000);
