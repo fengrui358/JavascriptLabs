a5125efff81d10d06aa64a87173243c93ea01513
fengrui <fengrui358@163.com>
2021-01-19 01:01:43 +0800

    测试访问vuex module中的数据

--- VueJsLabs/vuex-lab/vuex-complex-lab/src/views/About.vue
+++ VueJsLabs/vuex-lab/vuex-complex-lab/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <label>读取modul1中的SET_TESTTRE</label>
+      <span>{{ testStr }}</span>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState("module1", ["testStr"]),
+  }
+};
+</script>
--- VueJsLabs/vuex-lab/vuex-complex-lab/src/views/Home.vue
+++ VueJsLabs/vuex-lab/vuex-complex-lab/src/views/Home.vue
@@ -5,10 +5,22 @@
       <input v-model="testInfo" />
     </div>
     <div>
-      <label>读取modul1中的SET_TESTTRE</label>
+      <label>读取modul1中的testStr</label>
       <span>{{ testStr }}</span>
       <label style="margin-left: 15px">修改modul1中的SET_TESTTRE</label>
       <input @input="SET_TESTTRE($event.target.value)" />
+    </div>
+    <div>
+      <label>读取modul1中的innerObj</label>
+      <span>{{ innerObj }}</span>
+      <label style="margin-left: 15px">修改modul1中的SET_TESTTRE</label>
+      <input type="button" @click="changeInnerObj" value="修改对象" />
+    </div>
+    <div>
+      <label>读取modul1中的innerObj中的部分值</label>
+      <span>{{ innerObj != null ? (innerObj.testObj != null ? innerObj.testObj.a : null) : null }}</span>
+      <label style="margin-left: 15px">修改modul1中的SET_TESTTRE</label>
+      <input type="button" @click="changeInnerObjValue" value="修改指定值" />
     </div>
   </div>
 </template>
@@ -30,10 +42,31 @@ export default {
         this.$store.commit(rootStoreTypes.CHANGE_NAME, newVal);
       },
     },
-    ...mapState("module1", ["testStr"]),
+    ...mapState("module1", ["testStr", "innerObj"]),
+  },
+  mounted: function () {
+    this[module1StoreTypes.SET_INNEROBJ]({
+      test: "free",
+      testObj: { a: "free" },
+    });
   },
   methods: {
-    ...mapMutations("module1", [module1StoreTypes.SET_TESTTRE]),
+    ...mapMutations("module1", [
+      module1StoreTypes.SET_TESTTRE,
+      module1StoreTypes.SET_INNEROBJ,
+    ]),
+    changeInnerObj: function () {
+      //修改整个对象
+      this[module1StoreTypes.SET_INNEROBJ]({
+        test: "free2",
+        testObj: { a: "free2" },
+      });
+    },
+    changeInnerObjValue: function () {
+      //修改整个对象
+      console.log(this.$store);
+      //this.$store.module1.innerObj.testObj.a = "free3"
+    },
   },
 };
 </script>
