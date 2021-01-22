a88eafa3ac30df2e73b1f9c6168e796bc4c725ec
fengrui <fengrui358@163.com>
2021-01-23 00:12:34 +0800

    增加坐标距离计算

---
+++ JavascriptLabs/utils/calcDistance.js
@@ -0,0 +1,17 @@
+//计算两个坐标点间的距离，返回km
+function calcDistance(lat1, lng1, lat2, lng2) {
+    var radLat1 = lat1 * Math.PI / 180.0;
+    var radLat2 = lat2 * Math.PI / 180.0;
+    var a = radLat1 - radLat2;
+    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+        Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;// EARTH_RADIUS;
+    s = Math.round(s * 10000) / 10000;
+    return s;
+}
+
+(function () {
+    let distance = calcDistance(39.899106, 116.486733, 39.899189, 116.479259);
+    console.log(distance)
+})()
