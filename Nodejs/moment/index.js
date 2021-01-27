6853f5822e9f8f11c47030ef9d6a27218eaef326
fengrui <fengrui358@163.com>
2021-01-27 16:25:24 +0800

    更新格式

--- Nodejs/moment/index.js
+++ Nodejs/moment/index.js
@@ -9,3 +9,16 @@ console.log('x1', x1)
 let d = new Date()
 d.setHours(1)
 console.log(d)
+
+console.log('-----------------------------------------------')
+const start = moment((new Date()).setDate(1))
+console.log('start', start)
+const duration = moment.duration(moment(new Date()).diff(start))
+console.log('duration', duration, duration.asHours(), duration.minutes(), duration.hours())
+if (duration.asHours() > 0) {
+  let s = `${Math.floor(duration.asHours()).toString().padStart(2, '0')}:${duration.minutes().toString().padStart(2, '0')}`
+  console.log(s)
+} else {
+  let s = `${duration.minutes().toString().padStart(2, '0')}:${duration.seconds().toString().padStart(2, '0')}`
+  console.log(s)
+}
