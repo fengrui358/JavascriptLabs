599aeb41f5662122998fd55112579d536b554b86
free <fengrui358@163.com>
2021-04-22 22:14:18 +0800

    add file loader

---
+++ WebpackLabs/jikeshijian/17resolveimagefont/src/search.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './search.less';
+import logo from './images/logo.png'
+
+class Search extends React.Component {
+    render() {
+        return <div class="search-text">Search Text <img src={ logo } /></div>;
+    }
+}
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+)
