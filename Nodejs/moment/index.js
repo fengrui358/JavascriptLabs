5673b707306d39115907ff156998612cd2bd269b
fengrui <fengrui358@163.com>
2021-07-16 10:08:01 +0800

    测试 moment

--- Nodejs/moment/index.js
+++ Nodejs/moment/index.js
@@ -1,24 +1,25 @@
 let moment = require('moment')
 
 let now = moment()
-console.log(now.format())
+// console.log(now.format('YYYY-MM-DD HH:mm:ss'))
 
-let x1 = moment(new Date())
-console.log('x1', x1)
+// let x1 = moment(new Date())
+// console.log('x1', x1)
 
-let d = new Date()
-d.setHours(1)
-console.log(d)
+// let d = new Date()
+// d.setHours(1)
+// console.log(d)
 
 console.log('-----------------------------------------------')
-const start = moment((new Date()).setDate(1))
+const start = moment().minutes(28)
 console.log('start', start)
-const duration = moment.duration(moment(new Date()).diff(start))
-console.log('duration', duration, duration.asHours(), duration.minutes(), duration.hours())
-if (duration.asHours() > 0) {
-  let s = `${Math.floor(duration.asHours()).toString().padStart(2, '0')}:${duration.minutes().toString().padStart(2, '0')}`
-  console.log(s)
-} else {
-  let s = `${duration.minutes().toString().padStart(2, '0')}:${duration.seconds().toString().padStart(2, '0')}`
-  console.log(s)
-}
+const duration = start.diff(moment())
+console.log('duration', duration.asHours(), duration.asMinutes(), duration.hours())
+// console.log('duration', duration, duration.asHours(), duration.minutes(), duration.hours())
+// if (duration.asHours() > 0) {
+//   let s = `${Math.floor(duration.asHours()).toString().padStart(2, '0')}:${duration.minutes().toString().padStart(2, '0')}`
+//   console.log(s)
+// } else {
+//   let s = `${duration.minutes().toString().padStart(2, '0')}:${duration.seconds().toString().padStart(2, '0')}`
+//   console.log(s)
+// }
