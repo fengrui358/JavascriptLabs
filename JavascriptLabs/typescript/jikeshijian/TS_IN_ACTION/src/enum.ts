d8d82ca7ab9d3f533327414b70159e96a60c0bad
fengrui <fengrui358@163.com>
2021-08-24 23:37:20 +0800

    chore: make .prettierrc

--- JavascriptLabs/typescript/jikeshijian/TS_IN_ACTION/src/enum.ts
+++ JavascriptLabs/typescript/jikeshijian/TS_IN_ACTION/src/enum.ts
@@ -1,54 +1,52 @@
 // 数字枚举
 enum Role {
-    Reporter = 3,
-    /** 开发人员 */
-    Developer,
-    Maintainer,
-    Owner,
-    Guest
+  Reporter = 3,
+  /** 开发人员 */
+  Developer,
+  Maintainer,
+  Owner,
+  Guest
 }
 
 // 字符串枚举
 enum stringEnum {
-    Success = '成功',
-    Fail = '失败'
+  Success = '成功',
+  Fail = '失败'
 }
-
-
 
 // 异构枚举
 enum Answer {
-    N,
-    Y = 'Yes',
-    C = 5,
-    B
+  N,
+  Y = 'Yes',
+  C = 5,
+  B
 }
 
 enum Char {
-    // const
-    a,
-    b = Char.a,
-    C = 1 + 3,
-    // computed
-    d = Math.random(),
-    e = '1324'.length,
-    // 计算枚举后的值必须手动赋值
-    //f
+  // const
+  a,
+  b = Char.a,
+  C = 1 + 3,
+  // computed
+  d = Math.random(),
+  e = '1324'.length
+  // 计算枚举后的值必须手动赋值
+  //f
 }
 
 // 常量枚举
 const enum Month {
-    Jan = 1,
-    Feb,
-    Mar
+  Jan = 1,
+  Feb,
+  Mar
 }
 // 常量枚举会在编译阶段被移除
-const month = [Month.Jan, Month.Feb, Month.Mar]
+const month = [Month.Jan, Month.Feb, Month.Mar];
 
 export default {
-    Role,
-    stringEnum,
-    Answer,
-    Char,
-    month
-}
+  Role,
+  stringEnum,
+  Answer,
+  Char,
+  month
+};
