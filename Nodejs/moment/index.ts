621cc23cb88a3fd93241528e90b953bf05122db5
fengrui <fengrui358@163.com>
2021-08-29 23:54:06 +0800

    test moment

---
+++ Nodejs/moment/index.ts
@@ -0,0 +1,14 @@
+// const moment = require('moment');
+import moment from 'moment';
+
+// 1.1 从 Date 初始化
+let nowDate = new Date();
+console.log('1.1 从 date 初始化：', nowDate, moment(nowDate));
+
+// 1.2 直接初始化
+let nowMoment = moment();
+console.log('1.2 直接初始化：', nowMoment);
+
+// 1.3 从字符串初始化
+console.log('1.3 从字符串初始化', moment('1987/08/05 15:36:25'))
+
