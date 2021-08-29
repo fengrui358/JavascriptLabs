52d09f88ff9e951665dc42ce6f74028218080055
fengrui <fengrui358@163.com>
2021-08-29 16:07:21 +0800

    feat: test https

--- JavascriptLabs/typescript/jikeshijian/TS_REACT_MANUAL/build/webpack.dev.config.js
+++ JavascriptLabs/typescript/jikeshijian/TS_REACT_MANUAL/build/webpack.dev.config.js
@@ -1,10 +1,18 @@
 const webpack = require('webpack')
 module.exports = {
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                devtools: 'cheap-module-eval-source-map'
-            }
-        })
-    ]
+  devServer: {
+    // https: {
+    //   key: '../certs/key.pem',
+    //   cert: '../certs/cert.pem'
+    // },
+    https: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        devtools: 'cheap-module-eval-source-map'
+      }
+    })
+  ]
 }
