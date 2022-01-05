57ddd85a6d5c63ac2cfb80a6fcfb8d33d4df0c9d
free <qjdev@qq.com>
2022-01-05 19:31:16 +0800

    验证 watch 在一个 tick 中触发

--- VueJsLabs/vue-lab/vue-lab/src/views/WatchLab/index.vue
+++ VueJsLabs/vue-lab/vue-lab/src/views/WatchLab/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <p>deep 监控一个对象，一次性修改了对象的多个属性，deep 的 watch 只会触发一次</p>
+        <p>由以上的显示顺序可验证 watch 的响应也是在一个 tick 中</p>
         <el-button @click="changeInfo">changeInfo</el-button>
     </div>
 </template>
@@ -27,9 +28,16 @@ export default {
     },
     methods: {
         changeInfo() {
+            this.$nextTick(()=>{
+                console.log('开始设置值')
+            });
             this.form.age += 1
             this.form.name = this.form.name + this.form.age
             this.form.email = this.form.email + this.form.age
+            this.$nextTick(()=>{
+                console.log('结束设置值')
+                console.log('%c由以上的显示顺序可验证 watch 的响应也是在一个 tick 中', 'color: red')
+            });
         }
     }
 }
