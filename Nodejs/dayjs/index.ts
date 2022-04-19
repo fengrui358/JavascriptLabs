88e42cc77b3b855f86ace6fc64d5c6b5f43cc518
fengrui <fengrui358@163.com>
2022-04-19 23:11:05 +0800

    国际化

--- Nodejs/dayjs/index.ts
+++ Nodejs/dayjs/index.ts
@@ -1,10 +1,22 @@
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
+import localeData from 'dayjs/plugin/localeData'
+import locale_zh_cn from 'dayjs/locale/zh-cn'
+
+dayjs.locale(locale_zh_cn)
 dayjs.extend(duration)
+dayjs.extend(localeData)
 
 let format = dayjs().format()
 console.log(format);
 
 let timeSpan = dayjs.duration(990, 'm')
 console.log(timeSpan, timeSpan.minutes(), timeSpan.asMinutes(), timeSpan.format('HH:mm'));
+
+console.log(dayjs.locale());
+console.log(dayjs.weekdays());
+console.log(dayjs.weekdaysShort());
+console.log(dayjs.weekdaysMin());
+console.log(dayjs.monthsShort());
+console.log(dayjs.months());
 
